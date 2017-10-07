16c6bc7288d66f1e380a9c0db7756085fcf49c81
drew-marchione <32522919+drew-marchione@users.noreply.github.com>
2017-10-07 14:29:19 -0400

    Update Admin_Create_Back.sql

--- MySQL/Admin_Create_Back.sql
+++ MySQL/Admin_Create_Back.sql
@@ -1,8 +1,8 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `Admin_Create_Back`(
-	  IN exercise_name VARCHAR(45),
-    IN exercise_video VARCHAR(150),
-    IN exercise_description MEDIUMTEXT,
-    IN exercise_type VARCHAR(45)
+	IN exercise_name VARCHAR(45),
+	IN exercise_video VARCHAR(150),
+	IN exercise_description MEDIUMTEXT,
+	IN exercise_type VARCHAR(45)
 )
 BEGIN
     if ( SELECT EXISTS (SELECT 1 FROM back WHERE exercise_name = exerciseName) ) THEN
@@ -12,19 +12,19 @@ BEGIN
     ELSE
     
     	INSERT INTO back
-    (
-      exerciseName,
-      videoLink,
-      exerciseDescription,
-      exerciseType
-		)
-    values
-    (
-      exercise_name,
-      exercise_video,
-      exercise_description,
-      exercise_type
-		);
+	    (
+	      exerciseName,
+	      videoLink,
+	      exerciseDescription,
+	      exerciseType
+			)
+	    values
+	    (
+	      exercise_name,
+	      exercise_video,
+	      exercise_description,
+	      exercise_type
+			);
         
         END IF;
 END
