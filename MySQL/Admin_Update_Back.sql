fd65b88430a1f55314547477d17935298866d710
drew-marchione <32522919+drew-marchione@users.noreply.github.com>
2017-10-10 14:46:14 -0400

    Update Admin_Update_Back.sql

--- MySQL/Admin_Update_Back.sql
+++ MySQL/Admin_Update_Back.sql
@@ -1,16 +1,16 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `Admin_Update_Back`(
 	IN exerciseID INT,
 	IN exercise_name VARCHAR(45),
-    IN videoLink VARCHAR(150),
-    IN exercise_description MEDIUMTEXT,
-    IN exercise_type VARCHAR(45)
+    	IN videoLink VARCHAR(150),
+    	IN exercise_description MEDIUMTEXT,
+    	IN exercise_type VARCHAR(45)
 )
 BEGIN
-    if ( SELECT EXISTS (SELECT 1 FROM back WHERE exercise_name = exerciseName AND exerciseID != back_id) ) THEN
+    	if ( SELECT EXISTS (SELECT 1 FROM back WHERE exercise_name = exerciseName AND exerciseID != back_id) ) THEN
     
 		SELECT 'Workout Exists !!';
 	
-    ELSE
+    	ELSE
 		
 		UPDATE back
 		SET back.exerciseName = exercise_name, back.videoLink = videoLink, back.exerciseDescription = exercise_description, back.exerciseType = exercise_type
