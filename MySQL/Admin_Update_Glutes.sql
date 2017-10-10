26bf4999d13cc25fb83821fdf6501088041785c8
drew-marchione <32522919+drew-marchione@users.noreply.github.com>
2017-10-10 14:49:45 -0400

    Create Admin_Update_Glutes.sql

---
+++ MySQL/Admin_Update_Glutes.sql
@@ -0,0 +1,21 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Admin_Update_Glutes`(
+	IN exerciseID INT,
+	IN exercise_name VARCHAR(45),
+    IN videoLink VARCHAR(150),
+    IN exercise_description MEDIUMTEXT,
+    IN exercise_type VARCHAR(45)
+    
+)
+BEGIN
+    if ( SELECT EXISTS (SELECT 1 FROM glutes WHERE exercise_name = exerciseName AND exerciseID != glutes_id) ) THEN
+    
+		SELECT 'Workout Exists !!';
+	
+    ELSE
+		
+		UPDATE glutes
+		SET glutes.exerciseName = exercise_name, glutes.videoLink = videoLink, glutes.exerciseDescription = exercise_description, glutes.exerciseType = exercise_type
+		WHERE glutes.glutes_id = exerciseID;
+        
+	END IF;
+END
