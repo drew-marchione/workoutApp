b1316e793ba6d13842a1733528d838d7311b6697
drew-marchione <32522919+drew-marchione@users.noreply.github.com>
2017-10-10 13:58:41 -0400

    Create Admin_Delete_Chest.sql

---
+++ MySQL/Admin_Delete_Chest.sql
@@ -0,0 +1,12 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Admin_Delete_Chest`(
+  IN id INT,
+	IN _name VARCHAR(45),
+	IN videoLink VARCHAR(150),
+	IN description MEDIUMTEXT,
+	IN exercise_type VARCHAR(45)
+)
+BEGIN
+
+	DELETE FROM chest WHERE chest.chest_id = id AND chest.exerciseName = _name AND chest.videoLink = videoLink AND chest.exerciseDescription = description AND chest.exerciseType = exercise_type;
+
+END
