f612ce1dd3aa573ff931aa3723b5ebeb619cdc20
drew-marchione <32522919+drew-marchione@users.noreply.github.com>
2017-10-10 14:49:13 -0400

    Update Admin_Update_Chest.sql

--- MySQL/Admin_Update_Chest.sql
+++ MySQL/Admin_Update_Chest.sql
@@ -1,17 +1,17 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `Admin_Update_Chest`(
 	IN exerciseID INT,
 	IN exercise_name VARCHAR(45),
-    IN videoLink VARCHAR(150),
-    IN exercise_description MEDIUMTEXT,
-    IN exercise_type VARCHAR(45)
+    	IN videoLink VARCHAR(150),
+    	IN exercise_description MEDIUMTEXT,
+    	IN exercise_type VARCHAR(45)
     
 )
 BEGIN
-    if ( SELECT EXISTS (SELECT 1 FROM chest WHERE exercise_name = exerciseName AND exerciseID != chest_id) ) THEN
+    	if ( SELECT EXISTS (SELECT 1 FROM chest WHERE exercise_name = exerciseName AND exerciseID != chest_id) ) THEN
     
 		SELECT 'Workout Exists !!';
 	
-    ELSE
+    	ELSE
 		
 		UPDATE chest
 		SET chest.exerciseName = exercise_name, chest.videoLink = videoLink, chest.exerciseDescription = exercise_description, chest.exerciseType = exercise_type
